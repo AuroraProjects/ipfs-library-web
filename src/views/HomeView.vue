6b84c09aabfb1229f3fb0b0ad947113710b7b86c
qianmianyao <zp1998421@gmail.com>
2022-03-05 18:32:33 +0800

    基础布局增加中

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
   <n-layout>
-    <n-layout-header>header</n-layout-header>
+    <n-layout-header>
+      <div class="header-left-logo">
+        <span>IPFSLibrary</span>
+      </div>
+      <div class="header-options">
+        <span class="menu">会员计划</span>
+        <span class="menu">会员计划</span>
+        <span class="menu">会员计划</span>
+        <span class="menu">会员计划</span>
+        <span class="menu">会员计划</span>
+      </div>
+    </n-layout-header>
     <n-layout-content content-style="padding: 24px;">
       content
     </n-layout-content>
@@ -23,15 +34,41 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.n-layout-header,
+.n-layout-header {
+  display: flex;
+  background: gray;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: calc(1130px + 8vw);
+  padding: 24px 4vw;
+  width: 100%;
+  align-items: center;
+  height: 80px;
+}
+
 .n-layout-footer {
   display: flex;
   background: #42b983;
-  padding: 24px;
 }
 
-.n-layout-sider {
-  background: rgba(128, 128, 128, 0.3);
+.header-left-logo {
+  display: flex;
+  flex: 2;
+}
+
+.header-options {
+  align-items: center;
+  display: flex;
+}
+
+.header-options {
+  font-weight: 700;
+  margin: 0 1.5rem;
+}
+
+.menu {
+  font-weight: 700;
+  margin: 0 1.5rem;
 }
 
 .n-layout-content {
