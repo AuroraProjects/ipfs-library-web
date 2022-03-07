035fff9560af50693500c25af1b128093fe94f54
qianmianyao <904121839@qq.com>
2022-03-07 13:45:26 +0800

    完成 header 布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,79 +1,125 @@
 <template>
-  <n-layout>
-    <n-layout-header>
-      <div class="header-left-logo">
-        <span>IPFSLibrary</span>
-      </div>
-      <div class="header-options">
-        <span class="menu">会员计划</span>
-        <span class="menu">会员计划</span>
-        <span class="menu">会员计划</span>
-        <span class="menu">会员计划</span>
-        <span class="menu">会员计划</span>
-      </div>
-    </n-layout-header>
-    <n-layout-content content-style="padding: 24px;">
-      content
-    </n-layout-content>
-    <n-layout-footer>footer</n-layout-footer>
-  </n-layout>
+<n-layout>
+  <n-layout-header>
+    <div class="logo">IPFSLibrary</div>
+    <div class="menu">
+      <nav>
+        <n-button class="button-text" text strong>使用说明</n-button>
+        <n-button class="button-text" text strong>常见问题</n-button>
+        <n-button class="button-text" text strong>开源地址</n-button>
+        <n-button class="button-text" text strong>
+          <n-icon>
+            <search />
+          </n-icon>
+        </n-button>
+      </nav>
+    </div>
+    <div class="login">
+      <n-button text strong>Login</n-button>
+      <n-button class="register" text-color="#FF3987" text strong>Register</n-button>
+    </div>
+  </n-layout-header>
+  <n-layout-content>
+    内容
+  </n-layout-content>
+  <n-layout-footer>
+    底部
+  </n-layout-footer>
+</n-layout>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { NLayout, NLayoutHeader, NLayoutContent, NLayoutFooter } from 'naive-ui'
+import {
+  NLayout,
+  NLayoutHeader,
+  NLayoutContent,
+  NLayoutFooter,
+  NButton,
+  NIcon
+} from 'naive-ui'
+import { Search } from '@vicons/ionicons5'
 
 export default defineComponent({
   components: {
+    // 框架组件
     NLayout,
     NLayoutHeader,
     NLayoutContent,
-    NLayoutFooter
+    NLayoutFooter,
+    NButton,
+    NIcon,
+    // 图标
+    Search,
   }
 })
 </script>
 
 <style scoped>
+/*头部布局*/
 .n-layout-header {
   display: flex;
-  background: gray;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: calc(1130px + 8vw);
-  padding: 24px 4vw;
-  width: 100%;
-  align-items: center;
+  /*background: darkgray;*/
+  justify-content: space-between;
+  padding-left: 4vw;
+  padding-right: 4vw;
   height: 80px;
-}
-
-.n-layout-footer {
-  display: flex;
-  background: #42b983;
-}
-
-.header-left-logo {
-  display: flex;
-  flex: 2;
-}
-
-.header-options {
   align-items: center;
+}
+
+.logo {
+  color: darkgray;
+  font-size: 1.5rem;
+  font-weight: 800;
+}
+
+.n-layout-header div {
   display: flex;
+  /*background: #42b983;*/
+  height: 30px;
+  flex: 3;
+  align-items: center;
 }
 
-.header-options {
-  font-weight: 700;
-  margin: 0 1.5rem;
+/*头部右侧布局*/
+.login {
+  display: flex;
+  justify-content: flex-end;
 }
 
-.menu {
+.register {
+  margin-left: 2rem;
+}
+
+/*目录布局*/
+.menu{
+  display: flex;
+  width: 494px;
+  justify-content: center;
+}
+.menu nav {
+  display: flex;
+  /*background: antiquewhite;*/
+  height: 30px;
+  width: 494px;
+  justify-content: center;
+  align-items: center;
+}
+.button-text {
   font-weight: 700;
-  margin: 0 1.5rem;
+  margin: 0 0.5rem;
 }
 
 .n-layout-content {
   display: flex;
-  height: 100vh;
-  background: rgba(128, 128, 128, 0.4);
+  background: darksalmon;
+  height: 90vh;
+}
+
+/*底部布局*/
+.n-layout-footer {
+  display: flex;
+  background: #42b983;
+  height: 10vh;
 }
 </style>
