f1b33b3af34668bc4fd5cd2a9d6a5240137b2706
qianmianyao <904121839@qq.com>
2022-03-08 10:43:30 +0800

    去除遮罩

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
           <span class="mx-3.5">导航选项</span>
           <span class="mx-3.5">导航选项</span>
           <span class="mx-3.5">导航选项</span>
-          <n-icon @click="showMask" class="mx-3.5" size="20"><search /></n-icon>
+          <n-icon class="mx-3.5" size="20"><search /></n-icon>
       </div>
     </div>
 <!--    注册布局-->
@@ -22,7 +22,7 @@
           <a class="ml-3">登录</a>
         </div>
         <div class="flex">
-          <n-icon @click="showMask" class="mx-3.5 visible lg:hidden" size="20"><search/></n-icon>
+          <n-icon class="mx-3.5 visible lg:hidden" size="20"><search/></n-icon>
           <n-icon @click="menu" class="visible lg:hidden" size="20"><list /></n-icon>
         </div>
       </div>
@@ -36,16 +36,14 @@
     <a class="mt-2 accent-pink-600">注册</a>
     <span class="mt-2">登录</span>
   </div>
-    <!-- 外层的遮罩 -->
-    <div class="mask-cover" v-if="isShow" @click="closeByMask" >
-    </div>
-  <n-layout-content class="content h-14">
+  <n-layout-content class="content">
     内容布局
-    <br/>
-    <button class="rounded-sm bg-blue-300 ring-2 w-12 h-14">button</button>
+    <n-input round placeholder="中" />
+    <n-button type="success" class="">
+      Primary
+    </n-button>
   </n-layout-content>
   <n-layout-footer>
-    底部
   </n-layout-footer>
 </n-layout>
 </template>
@@ -58,7 +56,8 @@ import {
   NLayoutContent,
   NLayoutFooter,
   NIcon,
-  // NInput
+  NInput,
+  NButton
 } from 'naive-ui'
 import {
   Search,
@@ -73,81 +72,22 @@ export default defineComponent({
     NLayoutContent,
     NLayoutFooter,
     NIcon,
+    NInput,
+    NButton,
     // NInput,
     // 图标
     Search,
-    List
+    List,
   },
-  props: {
-    //是否可以点击遮罩关闭，默认是
-    closeOnClickModal: {
-      type: Boolean,
-      default: true,
-    }
-  },
-  setup (props) {
+  setup () {
     const menuVisible = ref(false)
     const menu = () => {
       menuVisible.value = !menuVisible.value
     }
-    let isShow = ref(false);
-
-    //打开遮罩，由外部进行调用
-    let showMask = () => {
-      isShow.value = true;
-    };
-
-    //关闭遮罩
-    let closeMask = () => {
-      isShow.value = false;
-    };
-
-    //通过点击遮罩关闭
-    let closeByMask = () => {
-      if (props.closeOnClickModal) {
-        isShow.value = false;
-      }
-    };
     return {
       menuVisible,
-      menu,
-      showMask,
-      closeMask,
-      closeByMask,
-      isShow
+      menu
     }
   }
 })
 </script>
-
-<style scoped lang="scss">
-.mask-cover {
-  background: rgba($color: #121212, $alpha: 0.5);
-  position: fixed;
-  z-index: 1;
-  // 设置top、left、宽高保证全屏遮罩
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-}
-
-//内容层，z-index要大于遮罩层，确保内容在遮罩上显示
-.mask-content {
-  position: relative;
-  z-index: 2;
-  height: 50px;
-  margin: 5rem auto 0;
-}
-
-//动画
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     screens: {
       'md': {'min': '375px', 'max': '800px'},
-      'lg': {'min': '800px', 'max': '1535px'},
+      'lg': {'min': '800px'},
     }
   },
   plugins: [],
