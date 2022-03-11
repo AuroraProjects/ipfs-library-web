b2ac9f0534033103c68fa3d9d7ffc468ee4497ab
qianmianyao <904121839@qq.com>
2022-03-11 16:37:32 +0800

    拆分头部组件

---
+++ src/components/HerderView.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="flex-none ml-3 lg:mr-8 flex-1 md:flex-2">
+    <p class="flex text-base h-7 items-center font-black">IPFSLibrary</p>
+  </div>
+  <!--目录布局-->
+  <div class="flex-grow flex-1 max-w-sm min-w-max md:hidden">
+    <nav class="flex justify-center flex-nowrap items-center font-semibold">
+      <a class="mx-3.5" href="#">导航选项</a>
+      <a class="mx-3.5" href="#">导航选项</a>
+      <a class="mx-3.5" href="#">导航选项</a>
+      <a class="mx-3.5" href="#">导航选项</a>
+      <button class="flex items-center">
+        <n-icon class="mx-3.5" size="20" @click="search"><search /></n-icon>
+      </button>
+    </nav>
+  </div>
+  <!--    注册布局-->
+  <div class="flex-none mr-3 lg:mr-8 flex-1 md:flex-none text-base">
+    <div class="flex justify-end h-7 items-center">
+      <div class="visible md:hidden">
+        <a href="#" class="text-pink-500">Register</a>
+        <a href="#" class="ml-3">Login</a>
+      </div>
+      <div class="flex">
+        <button class="flex items-center">
+          <n-icon class="visible lg:hidden" size="20" @click="search"
+          ><search
+          /></n-icon>
+        </button>
+        <div @click="menuAction" class="lg:hidden burger">
+          <div class="burger-bar"></div>
+          <div class="burger-bar"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mobile-menu lg:hidden">
+    <div class="container">
+      <nav>
+        <a style="margin-top: 0" href="#">导航选项</a>
+        <a href="#">导航选项</a>
+        <a href="#">导航选项</a>
+        <a href="#">导航选项</a>
+      </nav>
+    </div>
+  </div>
+  <!-- 搜索框遮罩-->
+  <div class="modal-overlay" style="display: block">
+    <div class="modal modal-search">
+      <!--搜索框-->
+      <div class="search-from">
+        <input class="ml-5" placeholder="你想要什么?">
+        <n-button style="background-color: #FF79AE" color="#FF79AE">搜索</n-button>
+      </div>
+    </div>
+    <n-icon class="modal-close" size="24" color="#000" @click="modalClose"><close/></n-icon>
+  </div>
+</template>
+
+<script>
+import {
+  Search,
+  Close
+} from '@vicons/ionicons5'
+import { defineComponent } from 'vue'
+import {
+  NIcon,
+  NButton
+} from 'naive-ui'
+export default defineComponent({
+  components: {
+    Search,
+    NIcon,
+    NButton,
+    Close
+  },
+  props: {
+    modal: Boolean,
+    menu: Boolean
+  },
+  emits: ['updateModal', 'closeModal', 'menuAction'],
+  setup(props, { emit }) {
+    const search = () => {
+      emit('updateModal', true)
+    }
+    const modalClose = () => {
+      emit('closeModal', false)
+    }
+    let close = false
+    const menuAction = () => {
+      close = !close
+      emit('menuAction', close)
+    }
+    return {
+      search,
+      modalClose,
+      menuAction
+    }
+  }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,13 @@
 <template>
   <n-layout :class="{'modal-opened': modal, 'menu-opened': menu}">
     <n-layout-header class="header">
-      <div class="flex-none ml-3 lg:mr-8 flex-1 md:flex-2">
-        <p class="flex text-base h-7 items-center font-black">IPFSLibrary</p>
-      </div>
-      <!--目录布局-->
-      <div class="flex-grow flex-1 max-w-sm min-w-max md:hidden">
-        <nav class="flex justify-center flex-nowrap items-center font-semibold">
-          <a class="mx-3.5" href="#">导航选项</a>
-          <a class="mx-3.5" href="#">导航选项</a>
-          <a class="mx-3.5" href="#">导航选项</a>
-          <a class="mx-3.5" href="#">导航选项</a>
-          <button class="flex items-center">
-            <n-icon class="mx-3.5" size="20" @click="search"><search /></n-icon>
-          </button>
-        </nav>
-      </div>
-      <!--    注册布局-->
-      <div class="flex-none mr-3 lg:mr-8 flex-1 md:flex-none text-base">
-        <div class="flex justify-end h-7 items-center">
-          <div class="visible md:hidden">
-            <a href="#" class="text-pink-500">Register</a>
-            <a href="#" class="ml-3">Login</a>
-          </div>
-          <div class="flex">
-            <button class="flex items-center">
-              <n-icon class="visible lg:hidden" size="20" @click="search"
-                ><search
-              /></n-icon>
-            </button>
-            <div @click="menuAction" class="lg:hidden burger">
-              <div class="burger-bar"></div>
-              <div class="burger-bar"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="mobile-menu lg:hidden">
-        <div class="container">
-          <nav>
-            <a style="margin-top: 0" href="#">导航选项</a>
-            <a href="#">导航选项</a>
-            <a href="#">导航选项</a>
-            <a href="#">导航选项</a>
-          </nav>
-        </div>
-      </div>
+      <HerderView
+          :modal="modal"
+          :menu="menu"
+          @updateModal="updateModal"
+          @closeModal="closeModal"
+          @menuAction="menuAction"
+      ></HerderView>
     </n-layout-header>
     <!--  内容布局-->
     <n-layout-content class="content">
@@ -134,21 +96,11 @@
         <a class="menu-item" href="#">导航</a>
       </nav>
     </n-layout-footer>
-    <!-- 搜索框遮罩-->
-    <div class="modal-overlay" style="display: block">
-      <div class="modal modal-search">
-    <!--搜索框-->
-        <div class="search-from">
-          <input class="ml-5" placeholder="你想要什么?">
-          <n-button style="background-color: #FF79AE" color="#FF79AE">搜索</n-button>
-        </div>
-      </div>
-      <n-icon class="modal-close" size="24" color="#000" @click="modalClose"><close/></n-icon>
-    </div>
   </n-layout>
 </template>
 
 <script>
+import HerderView from "@/components/HerderView"
 import { defineComponent, ref } from "vue";
 import {
   NLayout,
@@ -160,17 +112,16 @@ import {
   NButton,
 } from "naive-ui";
 import {
-  Search,
   Star,
   ChevronForward,
   LogoGithub,
   LogoVue,
-  LogoTwitter,
-  Close
+  LogoTwitter
 } from "@vicons/ionicons5";
 
 export default defineComponent({
   components: {
+    HerderView,
     // 框架组件
     NLayout,
     NLayoutHeader,
@@ -180,33 +131,32 @@ export default defineComponent({
     NAvatar,
     NButton,
     // 图标
-    Search,
     Star,
     ChevronForward,
     LogoGithub,
     LogoVue,
-    LogoTwitter,
-    Close
+    LogoTwitter
   },
   setup() {
     const modal = ref(false)
-    const search = () => {
-      modal.value = true
+    // 下发子组件的数据
+    const updateModal = (bool) => {
+      modal.value = bool
     }
-    const modalClose = () => {
-      modal.value = false
+    const closeModal = (bool) => {
+      modal.value = bool
     }
     const menu = ref(false)
-    const menuAction = () => {
-      menu.value = !menu.value
+    const menuAction = (bool) => {
+      menu.value = bool
     }
     return {
-      search,
       modal,
-      modalClose,
       menu,
-      menuAction
-    };
-  },
-});
+      menuAction,
+      updateModal,
+      closeModal
+    }
+  }
+})
 </script>
